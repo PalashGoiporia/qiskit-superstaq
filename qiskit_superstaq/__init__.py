9f0f733d043072aeb8436d63600fec93e9f956f1
Pranav Gokhale <pranav@super.tech>
2022-04-07 08:49:01 -0500

    import SuperstaQJob/Backend/Provider in superstaq/__init__.py
    Closes #137

--- qiskit_superstaq/__init__.py
+++ qiskit_superstaq/__init__.py
@@ -1,10 +1,10 @@
 from ._init_vars import API_URL, API_VERSION
 from . import compiler_output, serialization  # noqa: I100; b/c ._init_vars need to be init first
-from . import superstaq_backend
-from . import superstaq_job
-from . import superstaq_provider
 from ._version import __version__
 from .custom_gates import AceCR, ParallelGates, ZZSwapGate
+from .superstaq_backend import SuperstaQBackend
+from .superstaq_job import SuperstaQJob
+from .superstaq_provider import SuperstaQProvider
 
 __all__ = [
     "AceCR",
@@ -13,9 +13,9 @@ __all__ = [
     "compiler_output",
     "ParallelGates",
     "serialization",
-    "superstaq_backend",
-    "superstaq_job",
-    "superstaq_provider",
+    "SuperstaQBackend",
+    "SuperstaQJob",
+    "SuperstaQProvider",
     "ZZSwapGate",
     "__version__",
 ]
